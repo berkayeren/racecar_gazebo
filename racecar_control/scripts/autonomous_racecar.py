663470b19ef122ffcbc75a087ad045ba62806c47
Berkay Eren <berkay.eren@teamdefinex.com>
2024-05-25 23:12:18 +0300

    Improvements.

--- racecar_control/scripts/autonomous_racecar.py
+++ racecar_control/scripts/autonomous_racecar.py
@@ -11,13 +11,13 @@ class AutonomousRacecar:
         rospy.init_node('autonomous_racecar', anonymous=True)
 
         # Publisher for Ackermann drive commands
-        self.drive_pub = rospy.Publisher('/vesc2/ackermann_cmd_mux/input/teleop', AckermannDriveStamped, queue_size=10)
+        self.drive_pub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/teleop', AckermannDriveStamped, queue_size=10)
 
         # Subscriber for laser scan data
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
 
         # Publisher for odometry data
-        self.odom_pub = rospy.Publisher('/vesc2/odom', Odometry, queue_size=1)
+        self.odom_pub = rospy.Publisher('/vesc/odom', Odometry, queue_size=1)
 
         self.drive_msg = AckermannDriveStamped()
         self.rate = rospy.Rate(10)  # 10 Hz
